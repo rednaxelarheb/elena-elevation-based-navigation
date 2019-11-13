879bf242f778668fe57a9c80724d19efaaaf722d
Alexander Behr <behraster@gmail.com>
2019-11-13 15:10:58 -0500

    replaced gray route line with gradient and added parent folder to server.py path

--- Server/Server.py
+++ Server/Server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import json
+import sys
+sys.path.insert(-1, '../')
 
 from path_finding import download_graph
 from path_finding import solver
