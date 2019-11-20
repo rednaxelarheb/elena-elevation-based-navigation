a63b850d1777db2f1add2946883708b9db7dc6df
Bryce <bbodleygomes@umass.edu>
2019-11-20 16:47:16 -0500

    added schema validation to ensure valid json was passed into get_route()

--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, Response
 import json
 import sys
 # sys.path.insert(-1, '../')
-
+import jsonschema
 from path_finding import download_graph
 from path_finding import solver
 
@@ -38,61 +38,71 @@ length: FLOAT
 @app.route('/get_route', methods=['POST'])
 def get_route():
     input_data = request.get_json()
-    # TODO validate json passed in using jsonvalidator library?
-    latitude = float(input_data['start_address']['latitude'])
-    longitude = float(input_data['start_address']['longitude'])
-    radius = input_data['length'] / 2
-    unknown_parameter = 0.001 #input_data['max_or_minimize_change'] (the elevation change we are looking for)
+    ######
+    valid = validate_json_in(input_data)
+    if valid:
 
-    # TODO process input data and compute best route and return it for rendering
+        latitude = float(input_data['start_address']['latitude'])
+        longitude = float(input_data['start_address']['longitude'])
+        radius = input_data['length'] / 2
+        unknown_parameter = 0.001 #input_data['max_or_minimize_change'] (the elevation change we are looking for)
 
-    graph = download_graph.download_graph(latitude, longitude, radius)
-    res = solver.solver(graph, latitude, longitude, unknown_parameter, radius*2).solve()
-    print(res)
-    routes = {}
-    count = 0;
-    edge_sequence = graph.es
-    for route in res:
-        edges = route[0]
-        index = 0
-        path = []
+        # TODO process input data and compute best route and return it for rendering
 
-        for edge in edges:
-            if index == 0:
-                vertexid = edge_sequence[edge].source
-                #graph.vs[vertexid]
-                # elevation_change = graph.es[edge].attributes().get('grade') #elevation change
-                path.append({'total_elevation_change': route[1]})
-                path.append({'distance': route[2]})
-                point = {"latitude": graph.vs[vertexid].attributes().get('y'), "longitude": graph.vs[vertexid].attributes().get('x'), "gradient": graph.es[edge].attributes().get('grade')}
-                path.append(point)
-                vertexid2 = edge_sequence[edge].target
-                point = {"latitude": graph.vs[vertexid2].attributes().get('y'), "longitude": graph.vs[vertexid2].attributes().get('x'), "gradient": graph.es[edge].attributes().get('grade')}
-                path.append(point)
-                index+=2
-            else:
-                vertexid = edge_sequence[edge].target
-                point = {"latitude": graph.vs[vertexid].attributes().get('y'), "longitude": graph.vs[vertexid].attributes().get('x'), "gradient": graph.es[edge].attributes().get('grade')}
-                path.append(point)
-                index +=1
+        graph = download_graph.download_graph(latitude, longitude, radius)
+        res = solver.solver(graph, latitude, longitude, unknown_parameter, radius*2).solve()
+        print(res)
+        routes = {}
+        count = 0;
+        edge_sequence = graph.es
+        for route in res:
+            edges = route[0]
+            index = 0
+            path = []
 
-
-
-
-        count+=1
-        route_name = "route%d" % count
-        # print(path)
-
-        routes[route_name] = path #
+            for edge in edges:
+                if index == 0:
+                    vertexid = edge_sequence[edge].source
+                    #graph.vs[vertexid]
+                    # elevation_change = graph.es[edge].attributes().get('grade') #elevation change
+                    path.append({'total_elevation_change': route[1]})
+                    path.append({'distance': route[2]})
+                    point = {"latitude": graph.vs[vertexid].attributes().get('y'), "longitude": graph.vs[vertexid].attributes().get('x'), "gradient": graph.es[edge].attributes().get('grade')}
+                    path.append(point)
+                    vertexid2 = edge_sequence[edge].target
+                    point = {"latitude": graph.vs[vertexid2].attributes().get('y'), "longitude": graph.vs[vertexid2].attributes().get('x'), "gradient": graph.es[edge].attributes().get('grade')}
+                    path.append(point)
+                    index+=2
+                else:
+                    vertexid = edge_sequence[edge].target
+                    point = {"latitude": graph.vs[vertexid].attributes().get('y'), "longitude": graph.vs[vertexid].attributes().get('x'), "gradient": graph.es[edge].attributes().get('grade')}
+                    path.append(point)
+                    index +=1
 
 
 
 
-    # print("this function was called)")
-    # print(routes)
-    # print(res)
-    return jsonify(routes)
+            count+=1
+            route_name = "route%d" % count
+            # print(path)
 
+            routes[route_name] = path #
+
+
+        return jsonify(routes)
+    else: #invlaid json was passed in
+        return Response("{}", status=600, mimetype='application/json') #
+
+
+#helper function that ensures the data passed into get_route follows our schema
+def validate_json_in(input_data):
+    with open('schemas/get_route_schema.json') as json_file:
+        schema = json.load(json_file)
+        try:
+            jsonschema.validate(input_data, schema)
+            return True
+        except:
+            return False
 
 
 if __name__ == '__main__':
